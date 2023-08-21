f706663436e65a99c70b54cbb251bfa896dcff17
Piyush R Gone <136354796+piyushrgone@users.noreply.github.com>
2023-08-21 19:13:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,57 @@
+import streamlit as st
+
+# Streamlit app title and description
+st.title("YouTube Data Harvesting and Warehousing")
+st.write("Welcome to the YouTube Data app!")
+
+# Input field for entering YouTube channel ID
+channel_id = st.text_input("Enter YouTube Channel ID:", "")
+
+# Button to retrieve channel details
+if st.button("Retrieve Channel Details"):
+    st.write(f"Channel ID entered: {channel_id}")
+    # You can make API requests and display retrieved data here
+
+# Checkbox for channel migration
+migrate_channels = st.checkbox("Migrate Channels to Data Warehouse")
+
+# List of channels for migration
+channels_for_migration = st.multiselect("Select Channels for Migration:", ["Channel 1", "Channel 2", "Channel 3"])
+
+# Button to initiate migration
+if st.button("Migrate Selected Channels"):
+    if migrate_channels:
+        st.write(f"Migrating selected channels: {channels_for_migration}")
+        # You can write logic to migrate channel data to the data warehouse
+
+# Data visualization section
+st.header("Data Visualization")
+# You can add charts, tables, and graphs to visualize data here
+
+# Search options for querying SQL data warehouse
+st.header("SQL Data Warehouse Query")
+search_option = st.selectbox("Select Query Option:", ["Top Videos", "Channel Views", "Comments"])
+
+# Implement SQL query logic based on selected option
+if search_option == "Top Videos":
+    # Implement logic to retrieve top videos from SQL data warehouse
+    st.write("Displaying top videos...")
+elif search_option == "Channel Views":
+    # Implement logic to retrieve channel views from SQL data warehouse
+    st.write("Displaying channel views...")
+elif search_option == "Comments":
+    # Implement logic to retrieve comments from SQL data warehouse
+    st.write("Displaying comments...")
+
+# Provide a link to your GitHub repository
+st.sidebar.markdown("[GitHub Repository](https://github.com/yourusername/your-repo)")
+
+# Display project information and instructions
+st.sidebar.header("Project Information")
+st.sidebar.write("This Streamlit app allows users to interact with YouTube data.")
+st.sidebar.write("Enter a YouTube channel ID, retrieve details, and more.")
+
+# Display user's name (assuming you have access to it)
+user_name = "Piyush Gone"  # Replace with the user's name
+st.sidebar.write(f"Logged in as: {user_name}")
 
